4ce96b36697b553f6cfef4418d1024601f3b9849
ady <ady84.ro@gmail.com>
2023-04-27 22:50:24 +0300

    Add test upgrade buttons

--- script.js
+++ script.js
@@ -1,12 +1,56 @@
 const gameData = {
     totalAmount: 0,
-    incrementBtnPower: 1,
+    incrementButtonPower: 1,
 };
 
 const totalAmountText = document.querySelector("#total-amount");
-const incrementBtn = document.querySelector("#increment-btn");
+const incrementButton = document.querySelector("#increment-button");
 
-incrementBtn.addEventListener("click", () => {
-    gameData.totalAmount += gameData.incrementBtnPower;
-    totalAmountText.textContent = gameData.totalAmount;
+incrementButton.addEventListener("click", () => {
+    gameData.totalAmount += gameData.incrementButtonPower;
+    updateDisplay();
 });
+
+
+const upgradeData = {
+    upgradePowerCost: 5,
+    upgradePowerCost2: 8,
+};
+
+
+function addUpgradeListeners() {
+    const upgrades = document.querySelectorAll(".upgrade");
+    for (let i = 0; i < upgrades.length; i++) {
+        const upgradeButton = upgrades[i].firstElementChild;
+        upgradeButton.addEventListener("click", () => {
+            let cost = upgradeData[upgrades[i].lastElementChild.id];
+            gameData.totalAmount -= cost;
+
+            upgradeData[upgrades[i].lastElementChild.id] *= parseFloat(upgradeButton.dataset.costFactor);
+            
+            gameData[upgradeButton.dataset.upgrade] *= parseFloat(upgradeButton.dataset.upgradeFactor)
+
+            updateDisplay();
+        });
+    }
+}
+
+function displayUpgradeData() {
+    const upgrades = document.querySelectorAll(".upgrade");
+    for (let i = 0; i < upgrades.length; i++) {
+        let costTextElem = upgrades[i].lastElementChild;
+        let cost = upgradeData[costTextElem.id];
+        costTextElem.textContent = cost;
+    }
+}
+
+
+function updateDisplay() {
+    totalAmountText.textContent = gameData.totalAmount;
+    displayUpgradeData();
+}
+
+window.onload = () => {
+    updateDisplay();
+    addUpgradeListeners();
+}
